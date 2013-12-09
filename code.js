a089e92b132d235f0847f2bd949fd98a5894563a
Alex Bate <alex@alexbate.co.uk>
2013-12-09 11:26:12 +0000

    Removed parameter from js

--- code.js
+++ code.js
@@ -1,11 +1,5 @@
-function buttonClick(btn) {
+function buttonClick() {
   chrome.tabs.executeScript(null,
       {code:"document.title='" + document.getElementById("newTitle").value + "'"});
   window.close();
 }
-
-var el = document.getElementById("changeTitle");
-if (el.addEventListener)
-    el.addEventListener("click", doFunction, false);
-else if (el.attachEvent)
-    el.attachEvent('onclick', doFunction);
