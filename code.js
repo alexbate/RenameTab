cf3dfd9d828b5b589867187d6f07934c37f3bcba
Alex Bate <alex@alexbate.co.uk>
2013-12-09 12:55:19 +0000

    Works on enter key

--- code.js
+++ code.js
@@ -4,6 +4,15 @@ function buttonClick() {
   window.close();
 }
 
+
+function handleKeyPress(e){
+ var key=e.keyCode || e.which;
+  if (key==13){
+     buttonClick();
+  }
+}
+
 document.addEventListener('DOMContentLoaded', function () {
-      document.querySelector('button').addEventListener('click', buttonClick); 
+      document.querySelector('button').addEventListener('click', buttonClick);
+	  document.getElementById('newTitle').addEventListener('keypress', handleKeyPress);
 });
