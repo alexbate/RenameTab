a9a2da39085549ceeed00b4ef0fd9647308e08af
Alex Bate <alex@alexbate.co.uk>
2013-12-17 19:39:49 +0000

    Added CamTools

--- camtools.js
+++ camtools.js
@@ -1,5 +1,5 @@
 var url = location.href;
 var splitURL = url.split("/");
-var finalParse = splitURL[6].split(".");
+var finalParse = splitURL[splitURL.length-1].split(".");
 
 document.title=finalParse[0];
