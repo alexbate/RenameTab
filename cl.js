6b82f00ca46066cd17bae0a842478eb4816c8af3
Alex Bate <alex@alexbate.co.uk>
2013-12-23 11:47:51 +0000

    Will not auto-rename CL index pages

--- cl.js
+++ cl.js
@@ -1,5 +1,6 @@
 var url = location.href;
 var splitURL = url.split("/");
 var finalParse = splitURL[6].split(".");
-
-document.title=finalParse[0];
+if (finalParse[1]=='pdf') {
+	document.title=finalParse[0];
+}
