c72fce6dd73996c6b8d19db9b735f3ca7f7ff55a
Alex Bate <alex@alexbate.co.uk>
2013-12-27 13:22:31 +0000

    Added CompSci solutions

--- cl.js
+++ cl.js
@@ -1,6 +1,18 @@
 var url = location.href;
 var splitURL = url.split("/");
-var finalParse = splitURL[6].split(".");
-if (finalParse[1]=='pdf') {
-	document.title=finalParse[0];
+
+if (splitURL[5]=='pastpapers') {
+
+	var finalParse = splitURL[6].split(".");
+
+	if (finalParse[1]=='pdf') {
+		document.title=finalParse[0];
+	}
+	
+} else if (splitURL[5]=='solutions') {
+	
+	var finalParse = splitURL[7].split(".");
+	
+	var newTitle = "Solution - y" + splitURL[6] + finalParse[0];
+	document.title=newTitle;
 }
