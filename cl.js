657b426cc7da719f076a25016336b5b2591f94fc
Alex Bate <alex@alexbate.co.uk>
2014-01-14 12:56:06 +0000

    V1.3 - CS notes

--- cl.js
+++ cl.js
@@ -1,18 +1,25 @@
 var url = location.href;
 var splitURL = url.split("/");
+var finalParse = splitURL[splitURL.length-1].split(".");
 
-if (splitURL[5]=='pastpapers') {
+if (finalParse[1]=="pdf") {
+	if (splitURL[4]=='exams') {
+		if (splitURL[5]=='pastpapers') {
 
-	var finalParse = splitURL[6].split(".");
-
-	if (finalParse[1]=='pdf') {
-		document.title=finalParse[0];
+			if (finalParse[1]=='pdf') {
+				console.log(finalParse[0]);
+				var newTitle=finalParse[0];
+			}
+			
+		} else if (splitURL[5]=='solutions') {
+			
+			var finalParse = splitURL[7].split(".");
+			
+			var newTitle = "Solution - y" + splitURL[6] + finalParse[0];
+			
+		}
+	} else {
+	var newTitle = splitURL[5] + ' - ' + finalParse[0];
 	}
-	
-} else if (splitURL[5]=='solutions') {
-	
-	var finalParse = splitURL[7].split(".");
-	
-	var newTitle = "Solution - y" + splitURL[6] + finalParse[0];
-	document.title=newTitle;
 }
+document.title=newTitle;
