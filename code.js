8ed6f7c9a242af3e0a7b7acc8525af3034e431f0
Alex Bate <alex@alexbate.co.uk>
2014-03-27 14:50:24 +0000

    Added beta lock

--- code.js
+++ code.js
@@ -4,6 +4,12 @@ function buttonClick() {
   window.close();
 }
 
+function doInCurrentTab(tabCallback) { //http://stackoverflow.com/questions/7303452/how-to-get-current-tabid-from-background-page
+    chrome.tabs.query(
+        { currentWindow: true, active: true },
+        function (tabArray) { tabCallback(tabArray[0]); }
+    );
+}
 
 function handleKeyPress(e){
  var key=e.keyCode || e.which;
@@ -16,3 +22,17 @@ document.addEventListener('DOMContentLoaded', function () {
       document.querySelector('button').addEventListener('click', buttonClick);
 	  document.getElementById('newTitle').addEventListener('keypress', handleKeyPress);
 });
+
+function activateLock(setTitle) {
+	var currentTabID;
+	doInCurrentTab(function(tab){currentTabID=tab.id});
+
+	chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
+   		if (tabId !== currentTabID) {
+        	return false;
+    	}
+
+   		chrome.tabs.executeScript(null,
+      {code:"document.title='" + setTitle + "'"});
+	}); 
+}
